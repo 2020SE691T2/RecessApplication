7590c137bf91aa7598fe23a3fa15a308b1e5224f
Matthew Knobloch <msk358.drexel.edu>
2020-10-25 19:25:30 -0400

    update index.js so that it correctly imports

--- src/index.js
+++ src/index.js
@@ -3,8 +3,8 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker.js";
-import Recess from "./components/recess";
-ReactDOM.render(<Recess />, document.getElementById("root"));
+import Recess2 from "./Components/recess";
+ReactDOM.render(<Recess2 />, document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
