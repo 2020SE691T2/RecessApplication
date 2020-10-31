e8f69a3815eeba3401365de847aa9344d35d212e
Matthew Knobloch <msk358.drexel.edu>
2020-10-30 20:46:13 -0400

    chagne from full file path to relative

--- src/Components/CreateAccount.jsx
+++ src/Components/CreateAccount.jsx
@@ -1,8 +1,7 @@
 import React, { Component } from "react";
-import "/Users/pinkdove/RecessApplication/gui/src/CreateAccount.css";
+import "./CreateAccount.css";
 
 class CreateAccount extends Component {
-
 
   constructor(props) {
     super(props);
@@ -11,122 +10,122 @@ class CreateAccount extends Component {
       lastName: '',
       preferredName: '',
       email: '',
-      birthday:'',
+      birthday: '',
       password: ''
     };
 
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
-  
+
   }
 
-  handleChange (event) {
-    this.setState({firstName: event.target.firstName})
-    this.setState({lastName: event.target.lastName})
-    this.setState({prefferedName: event.target.preferredName})
-    this.setState({email: event.target.email})
-    this.setState({birthday: event.target.birthday})
-    this.setState({password: event.target.password})
+  handleChange(event) {
+    this.setState({ firstName: event.target.firstName })
+    this.setState({ lastName: event.target.lastName })
+    this.setState({ prefferedName: event.target.preferredName })
+    this.setState({ email: event.target.email })
+    this.setState({ birthday: event.target.birthday })
+    this.setState({ password: event.target.password })
   }
-  
- handleSubmit(event){
-   event.preventDefault();
- }
+
+  handleSubmit(event) {
+    event.preventDefault();
+  }
 
   render() {
 
-    const {firstName, lastName, email, birthday, prefferedName, password} = this.state;
+    const { firstName, lastName, email, birthday, prefferedName, password } = this.state;
     return (
       <div className="container">
         <body className="background">
-        
-      
-        
- 
-<div className ="header">
-         <p> <a href="https://recess-prototype.herokuapp.com/" target = "_blank"> <img src = "./Recess_logo.png"/></a>
-         </p>
-         
+
+
+
+
+          <div className="header">
+            <p> <a href="https://recess-prototype.herokuapp.com/" target="_blank"> <img src="./Recess_logo.png" /></a>
+            </p>
+
           </div>
 
-     
-
-<div className = "banner"> <p> <img src = "./signupbanner.png"/></p> </div>
 
 
+          <div className="banner"> <p> <img src="./signupbanner.png" /></p> </div>
 
 
 
-        <form>
 
-       
-        
-          
-           <input
+
+          <form>
+
+
+
+
+            <input
               className="firstName"
               type="text"
               placeholder="First Name:"
               value={this.state.firstName}
               onChange={this.handleChange}
-            /> 
+            />
 
-           
+
             <input
               className="lastName"
               type="text"
               placeholder="Last Name:"
               value={this.state.lastName}
               onChange={this.handleChange}
-            /> 
+            />
 
-<p></p>
-<p></p>
+            <p></p>
+            <p></p>
 
 
-  <input
+            <input
               className="email"
               type="text"
               placeholder="Email:"
               value={this.state.email}
               onChange={this.handleChange}
             />
-           
-           <input
-              className= "preferredName"
+
+            <input
+              className="preferredName"
               type="text"
               placeholder="Preffered Name:"
               value={this.state.prefferedName}
               onChange={this.handleChange}
-            /> 
-           <p></p>
-<p></p>
-             
-<input
-className="birthday"
+            />
+            <p></p>
+            <p></p>
+
+            <input
+              className="birthday"
               type="text"
               placeholder="Date of Birth"
               value={this.state.birthday}
               onChange={this.handleChange}
             />
-           
-           <input
-              className= "password"
+
+            <input
+              className="password"
               type="password"
               placeholder="Password:"
               value={this.state.password}
               onChange={this.handleChange}
-            /> 
-           <p></p>
-<p></p>
-             <input className="Submit" type="submit" value="" />     
-            
-          </form>  
+            />
+            <p></p>
+            <p></p>
+            <input className="Submit" type="submit" value="" />
 
-    
-           
+          </form>
 
 
-          
+
+
+
+
         </body>
       </div>
     );
