ebe5b3b95b3195fc80630a527138ce6b73ef07f2
Matthew Knobloch <msk358.drexel.edu>
2020-11-01 21:29:02 -0500

    fix code bugs/smells

--- src/Components/MenuBar.js
+++ src/Components/MenuBar.js
@@ -6,11 +6,11 @@ class Menubar extends React.Component {
         return (
             <div className="menuBar">
                 <ul id="bar">
-                    <left><a href="/">Home</a></left>
-                    <left><a href="/">About</a></left>
-                    <left><a href="/Profile">Profile</a></left>
-                    <right><a href="/login">Log In</a></right>
-                    <right><a href="/CreateAccount">Create Account</a></right>
+                    <li className="leftAlign"><a href="/">Home</a></li>
+                    <li className="leftAlign"><a href="/">About</a></li>
+                    <li className="leftAlign"><a href="/Profile">Profile</a></li>
+                    <li className="rightAlign"><a href="/login">Log In</a></li>
+                    <li className="rightAlign"><a href="/CreateAccount">Create Account</a></li>
                 </ul>
             </div>
         );
