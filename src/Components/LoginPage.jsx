37225181dab0ee90aee6ed6de3c2ab50d8de01c3
Matthew Knobloch <msk358.drexel.edu>
2020-11-29 17:28:22 -0500

    cleanup options for toastr

--- src/Components/LoginPage.jsx
+++ src/Components/LoginPage.jsx
@@ -68,21 +68,7 @@ class LoginPage extends Component {
           }
           else {
             toastr.options = {
-              "closeButton": true,
-              "debug": false,
-              "newestOnTop": false,
-              "progressBar": false,
-              "positionClass": "toast-top-right",
-              "preventDuplicates": false,
-              "onclick": null,
-              "showDuration": "300",
-              "hideDuration": "1000",
-              "timeOut": "5000",
-              "extendedTimeOut": "1000",
-              "showEasing": "swing",
-              "hideEasing": "linear",
-              "showMethod": "fadeIn",
-              "hideMethod": "fadeOut"
+              "closeButton": true
             }
             toastr.error('Incorrect username or password. Please try again.', 'Error');
           }
