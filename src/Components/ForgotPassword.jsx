3f71aa76bab074bdced97f33b2549f53a83bacc0
Matthew Knobloch <msk358.drexel.edu>
2021-01-24 15:27:26 -0500

    make the page responsive

--- src/Components/ForgotPassword.jsx
+++ src/Components/ForgotPassword.jsx
@@ -1,42 +1,37 @@
 import React, { Component } from "react";
 import "./ForgotPassword.css";
 import Menubar from "./MenuBar"
-
 // Bootstrap Components
-import Button from 'react-bootstrap/Button';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Form from 'react-bootstrap/Form';
 import Image from 'react-bootstrap/Image';
 
-
 class ForgotPassword extends Component {
 
-    constructor(props) {
-        super(props);
-        this.state = {
-          email: '',
-        };
+  constructor(props) {
+    super(props);
+    this.state = {
+      email: '',
+    };
 
-        this.changeemail= this.changeemail.bind(this);
+    this.changeEmail = this.changeEmail.bind(this);
+  }
 
-    }
-
-    changeemail(event) {
-        this.setState({ email: event.target.value });
-      }
+  changeEmail(event) {
+    this.setState({ email: event.target.value });
+  }
 
 
-      /* The code below may not be applicable to this program. I wasn't sure. */
+  /* The code below may not be applicable to this program. I wasn't sure. */
+  forgotPassword(event) {
+    event.preventDefault();
+    var json = JSON.stringify({
+      "email": this.state.email
+    });
 
-     forgotPassword(event) {
-        event.preventDefault();
-        var json = JSON.stringify({
-          "email": this.state.email
-        });
-
-        fetch("https://recess-api.herokuapp.com/users/", {
+    fetch("https://recess-api.herokuapp.com/users/", {
       method: "POST",
       body: json,
       headers: {
@@ -52,81 +47,49 @@ class ForgotPassword extends Component {
         }
       });
 
-    }
+  }
 
-/* Method above needs to be fixed. */
-
-
-    render(){
-
-
-        return (
-<div>
-
-<Menubar />
-
-<Container className="background_FP" fluid>
-
-<Form onSubmit={this.forgotPassword}>
-
-    <div className = "header">
-
-    <Row>
-                <Col>
-                   <Image src="./ForgotPasswordBanner.png" alt={'Forgot Password'} fluid />
-                </Col>
-              </Row>
-
-              <Row>
-                <Col>
-                   <Image src="./pwlock.png" alt={'Password Image'} fluid />
-                </Col>
-              </Row>
-
-
-    </div>
-   
-
-<Row >
-
-<Col md={5} >
-  <Form.Group controlId="emailFormGroup">
-    <Form.Control
-      className="forgotPasswordInput"
-      name= "email_fp"
-      type= "text"
-      placeholder="Enter your email here:"
-      value={this.state.email}
-      onChange={this.changeemail}
-      style={{ height: 64 }}
-    />
-  </Form.Group>
-
-</Col>
-
-
-
-
-
-</Row>
-
-<Row>
-
-<Col>
-  <input className="Submit_FP " type="submit" value="" />
-
-</Col>
-</Row >
-
-
-
-
-</Form>
-</Container>
-
-</div>
-
-        );
-    }
+  /* Method above needs to be fixed. */
+  render() {
+    return (
+      <div>
+        <Menubar />
+        <Container className="background_FP" fluid>
+          <Form onSubmit={this.forgotPassword}>
+            <Row>
+              <Col>
+                <Image src="./ForgotPasswordBanner.png" alt={'Forgot Password'} fluid />
+              </Col>
+            </Row>
+            <Row>
+              <Col>
+                <Image src="./pwlock.png" alt={'Password Image'} fluid />
+              </Col>
+            </Row>
+            <Row className="justify-content-md-center">
+              <Col md={5} >
+                <Form.Group controlId="emailFormGroup">
+                  <Form.Control
+                    className="forgotPasswordInput"
+                    name="email_fp"
+                    type="email"
+                    placeholder="Enter your email here:"
+                    value={this.state.email}
+                    onChange={this.changeEmail}
+                    style={{ height: 64 }}
+                  />
+                </Form.Group>
+              </Col>
+            </Row>
+            <Row className="justify-content-md-center">
+              <Col>
+                <input className="Submit_FP " type="submit" value="" />
+              </Col>
+            </Row >
+          </Form>
+        </Container>
+      </div>
+    );
+  }
 }
 export default ForgotPassword;
