83a7750e562328782a04bd660b98b9a857c1156b
Matthew Knobloch <msk358.drexel.edu>
2021-02-06 19:55:05 -0500

    add space above the profile picture on teh profile page

--- src/Components/ProfilePage.js
+++ src/Components/ProfilePage.js
@@ -172,6 +172,7 @@ class ProfilePage extends Component {
             <div>
                 <Menubar />
                 <Container fluid className={'backgroundProfilePage'}>
+                    <br />
                     <Row>
                         <Col>
                             <Image src={this.state.photo} fluid alt={'Profile Picture'} style={{ height: '150px', width: '150px' }} />
