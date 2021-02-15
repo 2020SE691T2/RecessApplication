c58c6f388ef4793e2a6c9939fe69bdfdbbe25ce6
wp86 <coxwm11@gmail.com>
2021-02-15 15:15:05 -0600

    roster additions

--- src/App.js
+++ src/App.js
@@ -22,6 +22,7 @@ import { createStore, combineReducers } from 'redux'
 import { reducer as toastrReducer } from 'react-redux-toastr'
 import Calendar from './Components/Calendar'
 import { Helmet } from 'react-helmet'
+import Roster from './Components/Roster';
 
 const reducers = {
   toastr: toastrReducer
@@ -51,6 +52,7 @@ function App() {
           <Route exact path="/Logout" component={LogoutPage} />
           <Route exact path="/Calendar" component={Calendar} />
           <Route exact path="/ChangePassword" component={ChangePassword} />
+          <Route exact path="/Roster" component={Roster} />
         </Switch>
       </BrowserRouter>
       <Provider store={store}>
--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ import "./index.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker.js";
 import 'bootstrap/dist/css/bootstrap.min.css';
+import $ from 'jquery';
+import Popper from 'popper.js';
 
 ReactDOM.render(<App />, document.getElementById("root"));
 
