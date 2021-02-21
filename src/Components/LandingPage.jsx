10247d65f181150e4cb0c082248b4246ac594d48
coxwm11 <coxwm11@gmail.com>
2021-02-21 17:52:59 -0600

    removed code smell

--- src/Components/LandingPage.jsx
+++ src/Components/LandingPage.jsx
@@ -5,7 +5,6 @@ import "./LandingPage.css";
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
-import Form from 'react-bootstrap/Form';
 import Image from 'react-bootstrap/Image';
 import Menubar from "./MenuBar";
 
