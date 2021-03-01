a78195db11225e8961d55bafc799f0406a15652f
Jonathan McDaniel <JonathanTMcDaniel@gmail.com>
2021-03-01 18:13:40 -0500

    view event page

--- src/Components/Environment.jsx
+++ src/Components/Environment.jsx
@@ -15,7 +15,7 @@ class Environment {
   }
 
   getRootUrl() {
-    let rootUrl = 'http://127.0.0.1:8000'
+    let rootUrl = 'https://recess-api.herokuapp.com'
     if (this.isHeroku()) {
       rootUrl = "https://recess-api.herokuapp.com";
     } 
